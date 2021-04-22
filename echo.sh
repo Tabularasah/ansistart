ad4481ea5948453d689e5b25cb69ad81257f028f
Tabularasah <50595939+Tabularasah@users.noreply.github.com>
2021-04-22 19:34:14 +0200

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -7,7 +7,7 @@ sudo apt update
 sudo apt install -y software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt update
-sudo apt install ansible
+sudo apt install -y ansible
 
 ## Bring in ansible files
 
